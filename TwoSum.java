ce958cae12224d6134638e9151d41e8be8d7f846
sandesh <bhujbalsandesh52@gmail.com>
2024-04-13 20:43:57 +0530

    add 1. Two Sum program

--- TwoSum.java
+++ TwoSum.java
@@ -1,27 +1,35 @@
-//1. Two Sum. 
+//1. Two Sum.
+
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
+// Example 1:
 
- 
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
 
-import java.util.HashMap;
-public class TwoSum 
-{
-    public int[] twoSum(int[] nums, int target)
-     {
-        HashMap<Integer,Integer> hashmap = new HashMap<Integer,Integer>();
-        for(int i=0; i<nums.length; i++)
-        {
-        Integer reNum = (Integer)(target-nums[i]);
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
 
-        if(hashmap.containsKey(reNum))
-        {
-            int toReturn[] = {hashmap.get(reNum),i};
-            return toReturn;
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] == target - nums[i]) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
         }
-        hashmap.put(nums[i], i);
-        }
-        return null;
-     }
+        return ans;
+    }
 }
