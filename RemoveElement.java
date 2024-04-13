4ca3659d4e1ad3141565b37f811de5e62edbbed2
sandesh <bhujbalsandesh52@gmail.com>
2024-04-13 20:48:19 +0530

    update codes

--- RemoveElement.java
+++ RemoveElement.java
@@ -7,11 +7,21 @@
 // Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
 // Return k.
 
+// Example 1:
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+// Example 2:
+// Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// Output: 5, nums = [0,1,4,0,3,_,_,_]
+
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
         int count = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] != val){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
                 nums[count++] = nums[i];
             }
         }
